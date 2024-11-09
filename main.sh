9d29b1c60d471664068ad6b103c4983f362f9d80
Arthur Chang <arthurc0102@gmail.com>
2024-11-09 10:09:11 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-read -p "Enter you branch: " branch
+# read -p "Enter you branch: " branch
 
-if [ "$branch" == "main" ] || [ "$branch" == "master" ]; then
-    echo "This is production"
-elif [ "$branch" == "develop" ]; then
-    echo "This is develop"
-else
-    echo "Unknwon branch"
-fi
+# if [ "$branch" == "main" ] || [ "$branch" == "master" ]; then
+#     echo "This is production"
+# elif [ "$branch" == "develop" ]; then
+#     echo "This is develop"
+# else
+#     echo "Unknwon branch"
+# fi
+
+echo "Hello ${username:-default}"
